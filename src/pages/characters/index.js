ab852fc0f23b056c1a83156a23a45e527c796f97
Rafael Santos <rafaelleonagraziele@gmail.com>
2024-05-14 22:13:52 -0300

    iniciando Segunda Pagina

---
+++ src/pages/characters/index.js
@@ -0,0 +1,13 @@
+// import { Container } from './styles';
+
+function characters() {
+  return (
+    <div>
+      <div>page 2</div>
+
+      <div>page 2</div>
+    </div>
+  );
+}
+
+export default characters;
