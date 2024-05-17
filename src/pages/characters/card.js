fecac761e9ae5bac60278f3596be855fd4a21376
rafael santos <rafaelleonagraziele@gmail.com>
2024-05-17 16:06:22 -0300

    modal inicio

--- src/pages/characters/card.js
+++ src/pages/characters/card.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from '../../styles/Card.module.css'
+import styles from "../../styles/Card.module.css";
 
 export default function Card({ pokemon }) {
   return (
     <div className={styles.Card}>
       <Image
-        src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"}
+        src={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        }
         width={120}
         height={120}
         alt={pokemon.name}
       />
       <p className={styles.id}>#{pokemon.id}</p>
       <h3 className={styles.title}>{pokemon.name}</h3>
-      <Link href={`/pokemon/${pokemon.id}`} passHref> 
-      <h1 className={styles.btn}>Detalhes</h1>    
+      <Link href={`/pokemon/${pokemon.id}`} passHref>
+        <h1 className={styles.btn}>Detalhes</h1>
       </Link>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import { Search } from "@/assets/icon/Search";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
 
+const Modal = ({ isVisible, onClose }) => {
+  if (!isVisible) return null;
+
+  return (
+    <div className={styles.modalBackdrop} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button onClick={onClose}>Close</button>
+        <p>Modal content goes here...</p>
+      </div>
+    </div>
+  );
+};
+
 export default function Home() {
   const router = useRouter();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const handleSearchButtonClick = () => {
     router.push("/characters");
   };
+
+  const handleInputClick = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.boxInput}>
-        <input className={styles.input} placeholder="Buscar" />
+        <input
+          className={styles.input}
+          placeholder="Buscar"
+          onClick={handleInputClick}
+        />
         <Search />
       </div>
       <button className={styles.button} onClick={handleSearchButtonClick}>
         All Pokemons
       </button>
+      <Modal isVisible={isModalVisible} onClose={handleCloseModal} />
     </div>
   );
 }
